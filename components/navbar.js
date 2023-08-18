539522ad735c95b9c6fbfe0d02a3d82d08baf904
Arthur <tuco1301@gmail.com>
2023-08-18 12:56:46 -0300

    fix: responsividade da navbar arrumada

--- components/navbar.js
+++ components/navbar.js
@@ -21,14 +21,14 @@ const Navbar = () => {
             <>
               <div className="flex flex-wrap items-center justify-between w-full lg:w-auto">
                 <Link href="/">
-                  <span className="flex items-center space-x-3 text-4xl font-medium text-gray-500 dark:text-gray-100">
+                  <span className="flex items-center space-x-2 text-3xl font-medium text-gray-500 dark:text-gray-100">
                     <span>
                       <Image
                         src="/img/logo.svg"
                         alt="N"
                         width="32"
                         height="32"
-                        className="w-16"
+                        className="w-12"
                       />
                     </span>
                     <span>Projeto Abacate</span>
